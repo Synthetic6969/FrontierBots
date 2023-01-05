81866d2ffdb04904c6abf96823eb9c9de40cc050
Synthetic6969 <unibat78@gmail.com>
2023-01-05 19:41:26 +0000

    Added grenadiers support for setrank

--- Library/HudsonManagerCommands/setrank.js
+++ Library/HudsonManagerCommands/setrank.js
@@ -51,9 +51,10 @@ module.exports = {
         const targetUsername = await RobloxHelper.getUsernameFromUserId(targetUserId)
 
         // Get rank
+        const hbcGroupRoles = await RobloxHelper.getGroupRoles(process.env.HBC_GROUP_ID)
         let options = []
         let oldRankName = ''
-        for (role of await RobloxHelper.getGroupRoles(process.env.HBC_GROUP_ID)) {
+        for (role of hbcGroupRoles) {
             if (role.rank == targetRank) oldRankName = role.name; 
             if (role.rank < authorRank && role.rank < 45 && role.rank > 1 && role.rank != targetRank) {
                 options.push({
@@ -82,36 +83,116 @@ module.exports = {
                 // Prevent memory leaks
                 client.off('interactionCreate', dropdownCallback)
 
-                const rank = dropdownInteraction.values[0]
-                
-                // Remove old roles
-                await targetMember.roles.remove( DiscordHelper.getRoleIdFromName(interaction.guild, oldRankName) )
-                await targetMember.roles.remove( DiscordHelper.getRoleIdFromName(interaction.guild, '[ ' + targetMember.nickname.split('[')[1].replace(']', ' ]') + ' NCO') )
-                await targetMember.roles.remove( DiscordHelper.getRoleIdFromName(interaction.guild, '[ ' + targetMember.nickname.split('[')[1].replace(']', ' ]') + ' COM') )
-                
-                // Add new roles
-                let roles = [ DiscordHelper.getRoleIdFromName(interaction.guild, rank) ]
-                if (rank == 'Corporal' || rank == 'Sergeant' || rank == 'Master Sergeant' || rank == 'Warrant Officer') {
-                    roles.push( DiscordHelper.getRoleIdFromName(interaction.guild, '[ ' + targetMember.nickname.split('[')[1].replace(']', ' ]') + ' NCO') )
-                } if (rank == 'Ensign') {
-                    roles.push( DiscordHelper.getRoleIdFromName(interaction.guild, '[ ' + targetMember.nickname.split('[')[1].replace(']', ' ]') + ' COM'), )
+                const newRole = dropdownInteraction.values[0]
+
+                const finaliseSetRank = async () => {
+                    // Remove old roles
+                    await targetMember.roles.remove( [
+                        DiscordHelper.getRoleIdFromName(interaction.guild, oldRankName),
+                        DiscordHelper.getRoleIdFromName(interaction.guild, '[ ' + targetMember.nickname.split('[')[1].replace(']', ' ]') + ' NCO'),
+                        DiscordHelper.getRoleIdFromName(interaction.guild, '[ ' + targetMember.nickname.split('[')[1].replace(']', ' ]') + ' COM')
+                    ])
+                    
+                    // Add new roles
+                    let roles = [ DiscordHelper.getRoleIdFromName(interaction.guild, newRole) ]
+                    if (newRole == 'Corporal' || newRole == 'Sergeant' || newRole == 'Master Sergeant' || newRole == 'Warrant Officer') {
+                        roles.push( DiscordHelper.getRoleIdFromName(interaction.guild, '[ ' + targetMember.nickname.split('[')[1].replace(']', ' ]') + ' NCO') )
+                    } if (newRole == 'Ensign') {
+                        roles.push( DiscordHelper.getRoleIdFromName(interaction.guild, '[ ' + targetMember.nickname.split('[')[1].replace(']', ' ]') + ' COM'), )
+                    }
+                    targetMember.roles.add( roles )
+
+                    // Set rank
+                    RobloxHelper.setRank(targetUserId, process.env.HBC_GROUP_ID, newRole)
+
+                    // Reply
+                    interaction.editReply({'embeds' : [ DiscordHelper.successEmbed('Success', `Successfully ranked ${targetMember} to ${newRole}`) ], components : [], ephemeral : true})
+                    // Log command
+                    const oldRole = hbcGroupRoles.filter(roleData => roleData.rank == targetRank)[0]?.name
+                    const newRank = hbcGroupRoles.filter(roleData => roleData.name == newRole)[0]?.rank
+                    const isPromotion = targetRank < newRank
+                    interaction.guild.channels.cache.get('811628563866320936').send({embeds : [
+                        new Discord.EmbedBuilder()
+                            .setTitle(isPromotion ? "__Promotion__" : "__Demotion__")
+                            .addFields([
+                                {
+                                    "name": "Ranked User",
+                                    "value": `> **ROBLOX:** [${targetUsername}](https://www.roblox.com/profile/${targetUserId})\n > **Discord:** <@${targetMember.id}>`
+                                },
+                                {
+                                    "name": "Rank Info",
+                                    "value": `> \`${oldRole}\` -> \`${newRole}\``
+                                },
+                                {
+                                    "name": "Ranking Officer",
+                                    "value": `> **ROBLOX:** [${authorUsername}](https://www.roblox.com/profile/${authorUserId})\n > **Discord:** <@${interaction.member.id}>`
+                                },
+                            ])
+                            .setColor(isPromotion ? "Green" : "Red")
+                            .setFooter({"text": authorUsername})
+                            .setTimestamp()
+                    ]})
                 }
-                targetMember.roles.add( roles )
 
-                // Set rank
-                RobloxHelper.setRank(targetUserId, process.env.HBC_GROUP_ID, rank)
+                // Check if AUX
+                const tokenisedNickname = targetMember.nickname.split(']').map(a => a.replace('[',''))
+                if (tokenisedNickname.length > 2 && tokenisedNickname[1] == "AUX") {
+                    const company = tokenisedNickname[0]
 
-                // Reply
-                dropdownInteraction.deferUpdate()
-                interaction.editReply({'embeds' : [ DiscordHelper.successEmbed('Success', `Successfully ranked ${targetMember} to ${rank}`) ], components : [], ephemeral : true})
-                // Log command
-                interaction.guild.channels.cache.get('811628563866320936').send({embeds : [
-                    new Discord.EmbedBuilder()
-                        .setTitle(`${targetUsername} was ranked to ${rank}`)
-                        .setDescription(`[${targetUsername}](https://www.roblox.com/profile/${targetUserId}) was ranked to ${rank} by [${authorUsername}](https://www.roblox.com/profile/${authorUserId})`)
-                        .setFooter({"text": authorUsername})
-                        .setTimestamp()
-                ]})
+                    let options = []
+                    for (role of interaction.guild.roles.cache) {
+                        if (role[1].name.includes("[ " + company + " ]") && !role[1].name.includes("NCO") && !role[1].name.includes("COM") && !role[1].name.includes("AUX")) {
+                            options.push({
+                                label: role[1].name,
+                                value: role[1].name
+                            })
+                        }
+                    }
+                    interaction.editReply({embeds: [ DiscordHelper.promptEmbed('This user is a member of the auxilliary. What platoon would you like to recruit them to?', false) ], components: [
+                        new Discord.ActionRowBuilder()
+                            .addComponents(
+                                new Discord.StringSelectMenuBuilder()
+                                    .setCustomId(`SET_RANK_AUX_${targetUserId}${authorUserId}${key}`)
+                                    .setPlaceholder('Select a platoon')
+                                    .setMinValues(1)
+                                    .setMaxValues(1)
+                                    .addOptions(options)
+                        )
+                    ], ephemeral: true})
+
+                    const auxDropdownCallback = async auxDropdownInteraction => {
+                        if (!auxDropdownInteraction.isStringSelectMenu()) return;
+
+                        if (auxDropdownInteraction.customId == `SET_RANK_AUX_${targetUserId}${authorUserId}${key}`) {
+                            // Prevent memory leaks
+                            client.off('interactionCreate', auxDropdownCallback)
+
+                            // Set nickname
+                            await targetMember.setNickname(`${auxDropdownInteraction.values[0].replace(/ /g, '')} ${targetUsername}`)
+
+                            // Set roles
+                            await targetMember.roles.remove( DiscordHelper.getRoleIdFromName(interaction.guild, '[ ' + company + ' ] [ AUX ]') )
+                            await targetMember.roles.add( DiscordHelper.getRoleIdFromName(interaction.guild, auxDropdownInteraction.values[0]) )
+
+                            // Finalise
+                            auxDropdownInteraction.deferUpdate()
+                            finaliseSetRank()
+                        }
+                    }
+
+                    client.on('interactionCreate', auxDropdownCallback)
+                    dropdownInteraction.deferUpdate()
+
+                    // 10 minute timeout
+                    setTimeout(() => {
+                        interaction.editReply({embeds : [DiscordHelper.failureEmbed('Set Rank Failed', `The request timed out.`)], ephemeral : true});
+                        client.off('interactionCreate', auxDropdownCallback)
+                        return;
+                    }, 1000*10*60)
+                } else {
+                    dropdownInteraction.deferUpdate()
+                    finaliseSetRank()
+                }
             }
         }
 
